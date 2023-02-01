245fdae435b67a9c7f234864b0e2ab816c221f47
ANIK115 <aniknj4@gmail.com>
2023-02-01 23:01:21 +0600

    zemaphore implemented

--- PThreads Synchronization Offline/Offline4NecessaryCodes/zemaphore/test-zem.sh
+++ PThreads Synchronization Offline/Offline4NecessaryCodes/zemaphore/test-zem.sh
@@ -2,6 +2,6 @@ echo "Running test-zem.c"
 g++ test-zem.c zemaphore.c -o test-zem -lpthread
 ./test-zem
 echo
-echo "Running test-toggle.c"
-g++ test-toggle.c zemaphore.c -o test-toggle -lpthread
-./test-toggle
+# echo "Running test-toggle.c"
+# g++ test-toggle.c zemaphore.c -o test-toggle -lpthread
+# ./test-toggle
